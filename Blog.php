542e74f29d3462666523a28b9173a032e50b3c29
OscarObrienHHS <95391126+OscarObrienHHS@users.noreply.github.com>
2023-09-04 12:45:12 +1200

    Add files via upload

---
+++ Blog.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Blog.css">
+    <Title>Oscar Obrien Photography</Title>
+</head>
+<body>
+    <div class="Sidebar">
+        <h1>Oscar Obrien Photography</h1>
+        <br>
+        <p>Home</p>
+        <p>Instagram</p>
+    </div>
+    <div class="Blogposts">
+        <a href=><div class="28/5/23">
+            <h1>A Windy Weekend | 28/5/23<h1>
+                <p>Hello World </p>
+        </div></a>
+    </div>
+</body>
+</html>
---
+++ Home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Home.css">
+    <Title>Oscar Obrien Photography</Title>
+</head>
+<body>
+    <div class="Home_page">
+        <div class="About">
+            <h1>Oscar Obrien Photography</h1>
+            <p> Hello this is an about me ashdgs ahsf ghshgf hsfgasjh hsfashfgshf. ajsfhgashfghfs I do stuff sometimesgjsgkljdgkdgs</p>
+            <div class="menu">
+                <a href="Instagram">Instagram</a></a><a href="Blog.php">Blog</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
