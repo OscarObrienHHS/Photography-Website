5cfeac53d0bc1aa6de1bacadd74b27b8aa06200b
OscarObrienHHS <95391126+OscarObrienHHS@users.noreply.github.com>
2023-10-09 13:06:03 +1300

    Add files via upload

---
+++ Website/Blog.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Blog.css">
+    <Title>Oscar Obrien Photography</Title>
+</head>
+<body>
+    <div class="Sidebar">
+        <h1>Oscar <br> Obrien <br> Photography</h1>
+        <ul>
+            <a href="Home.php"><li>Home</li></a>
+            <a href="Blog.php"><li>Blog</li></a>
+            <a href="https://linktr.ee/oscar_obrien?utm_source=linktree_admin_share"><li>Socials</li></a>
+        <ul>
+    </div>
+    <div class="Blogposts">
+        <a href="Blogpost1.php"><div class="Blogpost">
+            <h2> Windy Weekend | 11/01/2023<h2>
+                <img url="IMG_4080_2.jpg">
+                <p></p>
+        </div></a>
+        <a href=><div class="Blogpost">
+            <h2><h2>
+                
+        </div></a>
+    </div>
+</body>
+</html>
