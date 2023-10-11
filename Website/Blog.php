eb718449467ec49ff18f05718a5f7a79a4599115
OscarObrienHHS <95391126+OscarObrienHHS@users.noreply.github.com>
2023-10-12 11:06:25 +1300

    Add files via upload

--- Website/Blog.php
+++ Website/Blog.php
@@ -4,8 +4,73 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Blog.css">
     <Title>Oscar Obrien Photography</Title>
+    <style>
+    body,html{
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    font-family: sans-serif;
+    }
+    .Sidebar {
+        height: 100%; 
+        width: 15vw; 
+        position: fixed; 
+        z-index: 1; 
+        top: 0; 
+        left: 0;
+        color: rgb(36, 77, 119);
+        overflow-x: hidden; 
+        float: left;
+        text-decoration: none;
+        z-index: 2;
+    }
+    h1{
+        text-transform: uppercase;
+        text-align: left;
+        font-size: 1.5vw;
+        padding-left: 1vw;
+    }
+    ul{
+        text-align: left;
+        padding-left: 0.2vw;
+        list-style-type: none;
+        padding: 20px 10px 20px 10px;
+    }
+    li{
+        color: rgb(36, 77, 119);
+        text-decoration: none;
+        font-size: 1vw;
+        padding: 0.5vw;
+    }
+    .Gallery{
+        text-decoration: none;
+        width: 80%;
+        height: 100%;
+        margin: auto;
+    }
+    a{
+        text-decoration: none;
+        color: black;
+    }
+    p{
+        font-size: 0.8vw;
+        text-align: center;
+        text-transform: initial;
+    }
+    h2{
+        text-align: left;
+        font-size: 1.4vw;
+        margin: 1vw;
+    }
+    .Post{
+        
+        color: rgb(36, 77, 119);
+        text-transform: initial;;
+        z-index: 0;
+    }
+    </style>
 </head>
 <body>
     <div class="Sidebar">
@@ -16,14 +81,14 @@
             <a href="https://linktr.ee/oscar_obrien?utm_source=linktree_admin_share"><li>Socials</li></a>
         <ul>
     </div>
-    <div class="Blogposts">
-        <a href="Blogpost1.php"><div class="Blogpost">
+    <div class="Gallery">
+        <a href="Post.php"><div class="Post">
             <h2> Windy Weekend | 11/01/2023<h2>
-                <img url="IMG_4080_2.jpg">
-                <p></p>
+                
+                <p> Hello</p>
         </div></a>
-        <a href=><div class="Blogpost">
-            <h2><h2>
+        <a href="Post.php"><div class="Post">
+            <h2>Raglan | <h2>
                 
         </div></a>
     </div>
