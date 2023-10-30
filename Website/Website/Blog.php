4a9b87ca9c8f7fab3236bb4bea8576bd85abd4db
OscarObrienHHS <95391126+OscarObrienHHS@users.noreply.github.com>
2023-10-31 08:12:48 +1300

    Add files via upload

---
+++ Website/Website/Blog.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Blog.css">
+    <Title>Oscar Obrien Photography</Title>
+</head>
+<body>
+    <div class="Sidebar">
+        <h1>Oscar <br> Obrien <br> Photography</h1>
+        <ul>
+            <a href="Home.php"><li>Home</li></a>
+            <a href="Blog.php"><li>Blog</li></a>
+            <a href="https://linktr.ee/oscar_obrien?utm_source=linktree_admin_share"><li>Socials</li></a>
+        <ul>
+    </div>
+    <div class="Blogposts">
+        <h1> </h1>
+        <a href="Blogpost1.php"><div class="Blogpost">
+            <h2> Windy Weekend | 11/01/2023</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Donec ac odio tempor orci dapibus ultrices in iaculis. Pulvinar sapien et ligula ullamcorper. Lectus urna duis convallis convallis tellus. Est ante in nibh mauris cursus mattis. Imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada. Odio ut sem nulla pharetra diam sit amet nisl suscipit. Facilisi morbi tempus iaculis urna id. Interdum consectetur libero id faucibus nisl tincidunt eget nullam. Potenti nullam ac tortor vitae purus faucibus ornare suspendisse. Mauris rhoncus aenean vel elit scelerisque mauris pellentesque. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum lectus. Pharetra magna ac placerat vestibulum lectus mauris ultrices. Eleifend mi in nulla posuere sollicitudin aliquam. Feugiat in fermentum posuere urna nec tincidunt praesent semper feugiat. Adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna. Suspendisse in est ante in. Vel turpis nunc eget lorem dolor sed. Phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam. Commodo nulla facilisi nullam vehicula ipsum a arcu... </p>
+        </div></a>
+    </div>
+</body>
+</html>
